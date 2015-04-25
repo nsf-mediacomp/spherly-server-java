278de71794c2a50ef90e5dc05db5de2801af461d
katzen <ben.1plus1@gmail.com>
2015-04-24 19:08:59 -0500

    Added start client button

--- SpherlyServer/src/Main.java
+++ SpherlyServer/src/Main.java
@@ -1,27 +1,26 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.BindException;
+import java.net.URI;
 
-public class Main {
+public class Main implements ActionListener {
     public static int packetNumber = 0;
 
+    private JPanel listPane;
     private JTextPane myTextPane;
     private JScrollPane scrollPane;
+    private JButton clientButton;
     private JFrame mainFrame;
 
-    public Main() throws IOException {
-    	try{
-    	ClientHTTPServer clientServer = new ClientHTTPServer(8000);
-    	clientServer.start();
-    	}catch(IOException e){
-    		System.out.println("Failed to start client server... exiting");
-    		throw e;
-    		//System.exit(0);
-    	}
+    public Main() throws IOException{
     	
         mainFrame = new JFrame();
         mainFrame.setBounds(new Rectangle(new Dimension(500, 400)));
@@ -40,6 +39,10 @@ public class Main {
             }
         });
 
+        clientButton = new JButton("Start Client");
+        clientButton.setActionCommand("start_client");
+        clientButton.addActionListener(this);
+        
         myTextPane = new JTextPane();
         myTextPane.setBackground(Color.BLACK);
         myTextPane.setBackground(Color.WHITE);
@@ -48,9 +51,49 @@ public class Main {
 
         scrollPane = new JScrollPane(myTextPane);
         scrollPane.setBackground(Color.BLACK);
+        
+        JPanel buttonPanel = new JPanel(); // to center button
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+        buttonPanel.setBorder(new EmptyBorder(10,10,10,10));
+        buttonPanel.add(Box.createHorizontalGlue());
+        buttonPanel.add(clientButton);
+        buttonPanel.add(Box.createHorizontalGlue());
+        
+        listPane = new JPanel();
+        listPane.setLayout(new BoxLayout(listPane, BoxLayout.Y_AXIS));
+        listPane.add(buttonPanel);
+        listPane.add(scrollPane);
 
-        mainFrame.add(scrollPane);
+        mainFrame.add(listPane);
         mainFrame.setVisible(true);
+        
+    	try{
+	    	ClientHTTPServer clientServer = new ClientHTTPServer(8000);
+	    	clientServer.start();
+    	}catch(IOException e){
+	        this.printToCommandPrompt("Failed to start client server! Client will have to be started manually.", Color.RED);
+    	}
+    }
+    
+    
+    public static void openWebpage(URI uri) throws IOException {
+        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+            desktop.browse(uri);
+        }
+    }
+    
+	@Override
+    public void actionPerformed(ActionEvent event){
+    	// Open client button clicked
+    	if("start_client".equals(event.getActionCommand())){
+    		try{
+    			openWebpage(new URI("http://127.0.0.1:8000/index.html"));
+    		}catch(Exception e){
+    	        this.printToCommandPrompt("Could not open browser!", Color.RED);
+    	        this.printToCommandPrompt("Browse to http://127.0.0.1:8000/index.html to connect", Color.RED);
+    		}
+    	}
     }
 
     public void printToCommandPrompt(String msg, Color c) {
